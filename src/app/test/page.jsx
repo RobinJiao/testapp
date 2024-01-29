14aa9e2d4887faf0b661e4512f9297a58b125345
Robin Jiao <robin.jiao@163.com>
2024-01-29 23:29:55 +0800

    test client session -works but where to add the role? callback

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const TestInput = () => {
@@ -12,41 +13,10 @@ const TestInput = () => {
   const handleBlur = () => {
     setFocus(false);
   };
-  return (
-    <div className="w-1/2 m-4">
-      TestInput Page
-      <form className="my-4 pt-4">
-        Form 1
-        <div className="">
-          <input
-            id="my-input"
-            type="text"
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            className="block w-full p-2 pl-4 text-gray-700 border-b-2 border-gray-300 focus:border-b-2 focus:border-green-500 focus:text-gray-900"
-          />
-          <label
-            htmlFor="my-input"
-            className={`${focus ? activeLabel : inActiveLabel}`}
-          >
-            My Input
-          </label>
-        </div>
-      </form>
-      <form action="" className="my-4  pt-4 relative ">
-        Form 2
-        <div>
-          <label htmlFor="" className="absolute">
-            Input is in the label
-            <input
-              type="text"
-              className="block w-full p-2 pl-4 text-gray-700 border-b-2 focus:border-b-2 "
-            />
-          </label>
-        </div>
-      </form>
-    </div>
-  );
+
+  const session = useSession();
+  console.log(session);
+  return <div className="w-1/2 m-4">TestInput Page</div>;
 };
 
 export default TestInput;
