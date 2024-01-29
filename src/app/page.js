a258dceb127250cf12426c41fb297dfc7daabd3c
Robin Jiao <robin.jiao@163.com>
2024-01-29 23:49:52 +0800

    client session & role

--- src/app/page.js
+++ src/app/page.js
@@ -5,5 +5,17 @@ import Image from "next/image";
 export default function Home() {
   const session = useSession();
   console.log(session);
-  return <>NEXT 14 App! </>;
+  return (
+    <>
+      NEXT 14 App!
+      {session.data ? (
+        <>
+          <h1>{session.data.user.email}</h1>
+          <h2>{session.data.user.role}</h2>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
